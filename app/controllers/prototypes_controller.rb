c611e20d32528f5f7bab9d70daf9e42a4ccbb09f
ryosuke1005 <nm1014mn@gmail.com>
2021-02-07 16:24:11 +0900

    修正後

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -30,18 +32,17 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
     if prototype.update(prototype_params)
-      redirect_to prototype_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
     if prototype.destroy
+      redirect_to root_path
+    else
       redirect_to root_path
     end
   end
@@ -52,10 +53,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,5 @@
 class UsersController < ApplicationController
-
-  def index
-    @users = User.all
-  end
-
-  def create
-    @users = User.create(user_params)
-  end
-
-  def new
-    @users = User.new
-  end
-
   def show
     @user = User.find(params[:id])
-    @prototypes = @user.prototypes
-  end
-
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :profile, :occupation, :position)
   end
 end
